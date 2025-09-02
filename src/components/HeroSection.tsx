5abc1bf53c2ea3da9457a9c5be8be6fea94874af
Le1ndro-Augusto <229790124+hubb2025@users.noreply.github.com>
2025-09-02 14:43:43 -0300

    up

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ChevronRight, Play } from "lucide-react";
-import heroImage from "@/assets/hero-drone-removebg-preview.png";
+import heroImage from "@/assets/ChatGPT Image 30 de ago. de 2025, 19_11_34.png";
 import backgroundImage from "@/assets/agricultural-field-bg.jpg";
 
 const HeroSection = () => {
