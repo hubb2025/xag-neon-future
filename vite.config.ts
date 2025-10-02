009df146bbf716480fa44298031915a443485acc
Le1ndro-Augusto <229790124+hubb2025@users.noreply.github.com>
2025-10-02 14:26:40 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ⚠️ coloque aqui o NOME exato do repositório
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
